a87c46e5956e32f52a5b1da774af4c08aa6ad280
Dongri Jin <dongrify@gmail.com>
2017-10-12 22:58:46 +0900

    Add: python2

---
+++ python3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import json
+import urllib
+
+import os
+import requests
+
+
+def downloadfile(url, filePath):
+    try:
+        urllib.request.urlretrieve(url, filePath)
+    except:
+        print('\t url is error', filePath)
+
+url = 'http://api-app.smartisan.com/app/index.php?r=paperapi/index/list&client_version=2&limit=100&paper_id=0'
+result = requests.get(url).content
+jsonStr = json.loads(result)
+
+fileRoot = './sos/'
+if not os.path.exists(fileRoot):
+    os.makedirs(fileRoot)
+
+for item in jsonStr['data']:
+    download = item['url']
+    filePath = fileRoot + item['id'] + '.jpg'
+    downloadfile(download, filePath)
+    print('down ' + download)
